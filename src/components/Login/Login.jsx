468973417c9f0488a0f67cee5d116034ea12a7ce
Suprio-Das <priyamd507@gmail.com>
2025-02-05 23:00:37 +0600

    Login Function Setup and Got The Form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,4 +1,8 @@
 const Login = () => {
+    const handleLogin = e => {
+        e.preventDefault();
+        console.log(e.target)
+    }
     return (
         <div>
             <div className="hero bg-base-200 min-h-screen">
@@ -8,14 +12,14 @@ const Login = () => {
                     </div>
                     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
                         <div className="card-body">
-                            <fieldset className="fieldset">
+                            <form onSubmit={handleLogin} className="fieldset">
                                 <label className="fieldset-label">Email</label>
                                 <input type="email" className="input" placeholder="Email" />
                                 <label className="fieldset-label">Password</label>
                                 <input type="password" className="input" placeholder="Password" />
                                 <div><a className="link link-hover">Forgot password?</a></div>
-                                <button className="btn btn-neutral mt-4">Login</button>
-                            </fieldset>
+                                <button type="submit" className="btn btn-neutral mt-4">Login</button>
+                            </form>
                         </div>
                     </div>
                 </div>
