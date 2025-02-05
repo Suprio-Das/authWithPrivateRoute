790f44355754a4f61ebb9ac42bf96892c8b2bb7b
Suprio-Das <priyamd507@gmail.com>
2025-02-05 22:37:59 +0600

    Login Form Created Using DaisyUI

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,25 @@
 const Login = () => {
     return (
         <div>
-            <h1>This is login.</h1>
+            <div className="hero bg-base-200 min-h-screen">
+                <div className="hero-content flex-col w-96">
+                    <div className="text-center lg:text-left">
+                        <h1 className="text-xl font-bold">Login now!</h1>
+                    </div>
+                    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                        <div className="card-body">
+                            <fieldset className="fieldset">
+                                <label className="fieldset-label">Email</label>
+                                <input type="email" className="input" placeholder="Email" />
+                                <label className="fieldset-label">Password</label>
+                                <input type="password" className="input" placeholder="Password" />
+                                <div><a className="link link-hover">Forgot password?</a></div>
+                                <button className="btn btn-neutral mt-4">Login</button>
+                            </fieldset>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
