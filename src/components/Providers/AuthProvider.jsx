e893f7e7d62586127caacdcb8ec7e6004a3248ad
Suprio-Das <priyamd507@gmail.com>
2025-02-06 19:42:12 +0600

    Steps added for creating context inside the context file

--- src/components/Providers/AuthProvider.jsx
+++ src/components/Providers/AuthProvider.jsx
@@ -15,3 +15,15 @@ const AuthProvider = ({ children }) => {
 };
 
 export default AuthProvider;
+
+/**
+ * Step -1 : Create context. e.g. export const AuthContext = createContext(null); Set null as default value
+ * Step -2 : Then Create AuthContext Provider in the return of the component. E.g. <AuthContext.Provider value={set any values}></AuthContext.Provider>
+ * Wrap the components wants to share the context. E.g. 
+    <AuthProvider>
+      <RouterProvider router={router}></RouterProvider>
+    </AuthProvider>
+
+* Access the component inside the context component as children.
+* Now, use the context by useContext hook. E.g. const authInfo = useContext(AuthContext);
+*/
